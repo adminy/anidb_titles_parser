499e8b908d68cdc3a63bff89e61979732b1c91f8
Marin <adminy@Marins-iMac.local>
2019-02-08 21:39:20 +0000

    Now AID is integer, updated list file

--- titlesConvert.js
+++ titlesConvert.js
@@ -4,17 +4,18 @@ let anime_titles = {}, titles = []
 fs.readFile('anime-titles.dat', 'utf8', function(err, contents) {
 	let lines = contents.split("\n")
 	for(let i = 3; i < lines.length; i++) {
-		let line = lines[i].split("|")
-		if(line[0] in anime_titles) {
-			if(anime_titles[line[0]].indexOf(line[3]) === -1) {
-				anime_titles[line[0]] += "|"+line[3]
+		let line = lines[i].split("|"),
+		     aid = parseInt(line[0])
+		if(aid in anime_titles) {
+			if(anime_titles[aid].indexOf(line[3]) === -1) {
+				anime_titles[aid] += "|"+line[3]
 			}
 		} else
-			anime_titles[line[0]] = line[3]
+			anime_titles[aid] = line[3]
 	}
 	
 	for(let key in anime_titles)
-		titles.push([key, anime_titles[key]])
+		titles.push([parseInt(key), anime_titles[key]])
 	
 	var file = fs.createWriteStream('titles.json');
 		file.write(JSON.stringify(titles));
